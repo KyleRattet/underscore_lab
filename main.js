b4938e276f6d1b1f82d98a27955fbe452bfdfa37
KyleRattet <krattet@gmail.com>
2015-07-17 13:41:42 -0600

    added solutions to sample and difference

--- main.js
+++ main.js
@@ -1,137 +1,125 @@
-// //alert("test")
-//
-// var _ = {
-// ///average
-//   averageCalc: function (array) {   var total = 0;
-//   //for loop to add all of the numbers
-//       for (i=0; i <array.length; i++){
-//         total = total + array[i]
-//       }
-//       ///returns the value of total/how many in the array
-//       return (total/(array.length)) }
-//       ,
-// ///contains
-//   contains: function(array,num) {
-//       array.includes(num)
-//     }
-//     ,
-// ///first - returns the first element in an array
-//   first: function(array) {
-//     //find first index
-//     return array[0]
-//   }
-//   ,
-// ///last - returns last element in an array
-//   last: function (array) {
-//     return array.pop()
-//   }
-//   ,
-//
-// ///max - returns the maximum value in an array
-//   max: function (array) {
-//     ///loop container
-//     var lastNum = 0;
-//     for (var i = 0; i < array.length; i++) {
-//       if (array[i] > lastNum) {
-//         lastNum = array[i]
-//       }
-//     }
-//     return lastNum
-//   }
-//   ,
-//
-// ///min - returns the min. value in an array
-//   min: function (array) {
-//     ///for loop to compare number vs what is in the array
-//     var min = Infinity;
-//     for (var i = 0; i < array.length; i++) {
-//       //if array element is less than the last name,
-//       //set the array element to last number
-//       if (array[i] < min) {
-//         min = array[i]
-//       }
-//     }
-//     return min
-//   }
-//   ,
-//
-// //shuffle -- shuffle - Returns a shuffled copy of the list, using a version of
-// //the Fisher-Yates shuffle. Don't worry about implementing that exact shuffle
-// //alogrithm. Start by creating your own simple shuffle.
-//
-//   // shuffle: function (array) {
-//   //   for (var i = array.length - 1; i > 0; i--) {
-//   //     var j = Math.floor(Math.random()*(i+1));
-//   //     var temp = array[i];
-//   //     array[i]= array[j];
-//   //     array[j]= temp;
-//   //   }
-//   //   return array;
-//   // }
-//
-//   shuffle: function (array) {
-//     for (var i = 0; i < array.length; i--) {
-//       var indexRandom = Math.floor(Math.random()*(i+1));
-//       var temp = array[i];
-//       array[i]= array[indexRandom];
-//       array[indexRandom]= temp;
-//     }
-//     return array;
-//   }
-//
-// //shuffle function pseudo code
-// //1. start with an array
-// //2. make a copy of the array, so we aren't impacting the original array
-// //3. assign an array literal container (blank array)
-// //4. for loop to loop through each element of the array copy
-// //5. generate random index number
-// //6. grab an element from the original array with random index
-// //7. exit for loop
-// //8. return array
-//
-// shuffle: function (array) {
-//   var arrayCopy = array;
-//   var shuffled = [];
-//     while (arrayCopy.length > 0){
-//       var randomIndex = Math.floor(Math.random()*arrayCopy.length);
-//       shuffled.push(arrayCopy[randomIndex]);
-//       arrayCopy.splice(randomIndex, 1);
-//   }
-//   return shuffled;
-// }
-// //sample --  Produce a random sample from the list. Pass a number to return n
-// //random elements from the list. Otherwise a single random item will be returned.
-//   sample: function (array) {
-//
-//   }
-//
-// ,
-//
-// //difference -- returns the values from array that are not present in the other array.
-//   difference: function (array1, array2) {
-//     var same = [];
-//     var diff = [];
-//
-//     for (var i = 0; i < array1.length; i++) {
-//       return array[i]
-//     }
-//     }
-//
-//
-//
-//   ,
-// }
-//
-//
-// //shuffle notes (mike)
-//
-// // -get current index (deck.length)
-// // -enter loop (loop until no more elements remain in array)
-// //      - generate random number for index
-// //      - get current element
-// //      - swap current with random
-// //      - update current index
-// //      - return array
+var _ = {
+///average
+  averageCalc: function (array) {
+      var total = 0;
+  //for loop to add all of the numbers
+      for (i=0; i <array.length; i++){
+        total = total + array[i];
+      }
+      ///returns the value of total/how many in the array
+      return (total/(array.length));
+      },
+///contains
+  contains: function(array,num) {
+      array.includes(num)
+    }
+    ,
+///first - returns the first element in an array
+  first: function(array) {
+    //find first index
+    return array[0]
+  }
+  ,
+///last - returns last element in an array
+  last: function (array) {
+    return array.pop()
+  }
+  ,
+
+///max - returns the maximum value in an array
+  max: function (array) {
+    ///loop container
+    var lastNum = 0;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > lastNum) {
+        lastNum = array[i]
+      }
+    }
+    return lastNum
+  }
+  ,
+
+///min - returns the min. value in an array
+  min: function (array) {
+    ///for loop to compare number vs what is in the array
+    var min = Infinity;
+    for (var i = 0; i < array.length; i++) {
+      //if array element is less than the last name,
+      //set the array element to last number
+      if (array[i] < min) {
+        min = array[i]
+      }
+    }
+    return min
+  },
+
+
+ shuffle: function (array) {
+  var arrayCopy = array;
+  var shuffled = [];
+    while (arrayCopy.length > 0){
+      var randomIndex = Math.floor(Math.random()*arrayCopy.length);
+      shuffled.push(arrayCopy[randomIndex]);
+      arrayCopy.splice(randomIndex, 1);
+  }
+  return shuffled;
+  },
+//sample --  Produce a random sample from the list. Pass a number to return n
+//random elements from the list. Otherwise a single random item will be returned.
+sample : function (array, num) {
+  var shuffleArr = _.shuffle(array);
+  if (num == null) {
+    num = 1;
+  }
+  return shuffleArr.slice(0, num)
+},
+
+
+
+
+//shuffle -- shuffle - Returns a shuffled copy of the list, using a version of
+//the Fisher-Yates shuffle. Don't worry about implementing that exact shuffle
+//alogrithm. Start by creating your own simple shuffle.
+
+  // shuffle: function (array) {
+  //   for (var i = array.length - 1; i > 0; i--) {
+  //     var j = Math.floor(Math.random()*(i+1));
+  //     var temp = array[i];
+  //     array[i]= array[j];
+  //     array[j]= temp;
+  //   }
+  //   return array;
+  // }
+
+  // shuffle: function (array) {
+  //   for (var i = 0; i < array.length; i--) {
+  //     var indexRandom = Math.floor(Math.random()*(i+1));
+  //     var temp = array[i];
+  //     array[i]= array[indexRandom];
+  //     array[indexRandom]= temp;
+  //   }
+  //   return array;
+  // }
+
+//shuffle function pseudo code
+//1. start with an array
+//2. make a copy of the array, so we aren't impacting the original array
+//3. assign an array literal container (blank array)
+//4. for loop to loop through each element of the array copy
+//5. generate random index number
+//6. grab an element from the original array with random index
+//7. exit for loop
+//8. return array
+
+//shuffle notes (mike)
+
+// -get current index (deck.length)
+// -enter loop (loop until no more elements remain in array)
+//      - generate random number for index
+//      - get current element
+//      - swap current with random
+//      - update current index
+//      - return array
 //
 // var deck =  [
 //    '&#x1F0A1','&#x1F0A2','&#x1F0A3','&#x1F0A4','&#x1F0A5','&#x1F0A6','&#x1F0A7',
@@ -195,9 +183,9 @@
 // }
 //
 // console.log(oldestPerson(personalInfo));
-
-///longest word
-
+//
+// /longest word
+//
 // function longestWord(string) {
 //   //split up sting to form new array of each word
 //   var soloString = string.split(" ");
@@ -215,30 +203,30 @@
 // }
 //
 // console.log(longestWord("what am i hthhththtS"));
-
-
-//find max value of an array
-myArray = [3,5,7,10];
-
-function max(array) {
-  var maxNum = 0;
-  for (var i = 0; i < array.length; i++) {
-    if(array[i] > maxNum){
-      maxNum = array[i];
-    }
-  }
-  return maxNum;
-
-}
-
-console.log(max(myArray));
-
-function foo(){
-var bar = function() {
-    return 3;
-};
-return bar();
-var bar = function() {
-    return 8;
-};
-}
+//
+//
+// //find max value of an array
+// myArray = [3,5,7,10];
+//
+// function max(array) {
+//   var maxNum = 0;
+//   for (var i = 0; i < array.length; i++) {
+//     if(array[i] > maxNum){
+//       maxNum = array[i];
+//     }
+//   }
+//   return maxNum;
+//
+// }
+//
+// console.log(max(myArray));
+//
+// function foo(){
+// var bar = function() {
+//     return 3;
+// };
+// return bar();
+// var bar = function() {
+//     return 8;
+// };
+// }
